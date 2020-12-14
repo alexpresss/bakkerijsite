57065ee2d4cda1f99144b0fbd560b925721ff766
MaximiliaanM <maximiliaan.muylaert@student.hogent.be>
2020-12-14 23:02:34 +0100

    Assortiment img compression

--- js/assortiment.js
+++ js/assortiment.js
@@ -15,7 +15,7 @@ function loadHTML(json){
         list.forEach(item => {
             let html = '<div class="portfolio-item '+ element +' col-xs-12 col-sm-4 col-md-3">'+
             '<div class="recent-work-wrap">'+
-            '<img class="img-responsive" src="images/assortiment/full/'+item.foto+'" alt="">'+
+            '<img class="img-responsive" src="images/assortiment/min/'+item.foto+'" alt="">'+
             '<div class="overlay">'+
                 '<div class="recent-work-inner">'+
                 '<h3><a class="preview" href="images/assortiment/full/'+item.foto+'" rel="prettyPhoto">'+item.naam+'</a></h3>'+
